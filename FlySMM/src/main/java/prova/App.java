611f87e210bd694540eabf0c4b44d2370ec05f9d
Davide Bolzoni <davide.bolzoni59@gmail.com>
2017-02-10 12:49:51 +0100

    Change hibernate DB "Prova"

--- FlySMM/src/main/java/prova/App.java
+++ FlySMM/src/main/java/prova/App.java
@@ -13,45 +13,50 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
 import prova.data.Message;
+
 /**
  * Servlet implementation class App
  */
 public class App extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public App() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public App() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		configureUsingHibernateConfigXMLFile();
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
-	public static void configureUsingHibernateConfigXMLFile(){
+
+	public static void configureUsingHibernateConfigXMLFile() {
 		// Create configuration instance
 		Configuration configuration = new Configuration();
 		// Pass hibernate configuration file
 		configuration.configure("hibernate.cfg.xml");
 
 		// Since version 4.x, service registry is being used
-		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
-				applySettings(configuration.getProperties()).build();	
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
 
 		// Create session factory instance
 		SessionFactory factory = configuration.buildSessionFactory(serviceRegistry);
@@ -63,19 +68,17 @@ public class App extends HttpServlet {
 		session.getTransaction().begin();
 
 		// Print out all required information
-		System.out.println("Session Is Opened :: "+session.isOpen());
-		System.out.println("Session Is Connected :: "+session.isConnected());	
+		System.out.println("Session Is Opened :: " + session.isOpen());
+		System.out.println("Session Is Connected :: " + session.isConnected());
 
-		// Create message object
-		Message message = new Message();
-		message.setMessage("Hello World!");
-
-		// Save
-		session.save(message);
-
-		// Commit transaction
-		session.getTransaction().commit();
-
+		/*
+		 * // Create message object Message message = new Message();
+		 * message.setMessage("Hello World!");
+		 * 
+		 * // Save session.save(message);
+		 * 
+		 * // Commit transaction session.getTransaction().commit();
+		 */
 		System.exit(0);
 	}
 
