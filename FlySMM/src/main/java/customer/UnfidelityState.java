486b212baaa025020823a1e402c172781266fa36
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-02-15 13:49:05 +0100

    edit UnfidelityState

--- FlySMM/src/main/java/customer/UnfidelityState.java
+++ FlySMM/src/main/java/customer/UnfidelityState.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 public class UnfidelityState extends State {
 
 	private Date actualDateUnfidelity;
+	private Customer customer;
 
 	public UnfidelityState(FidelityCustomer c) {
 		super(c);
@@ -18,7 +19,7 @@ public class UnfidelityState extends State {
 		if ((getDateDiff(this.actualDateUnfidelity, c.startDate, TimeUnit.MINUTES) / (1000 * 60 * 60 * 24)) > (365
 				* 2)) {
 			if (this.c instanceof FidelityCustomer){
-			//this.c =new  Customer(c);
+				this.customer = new Customer(c);
 			}
 		} else {
 			c.setFidelity(new FidelityState(c));
