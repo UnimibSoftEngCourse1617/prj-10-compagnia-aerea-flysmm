0bd9af052cbde2d11a434798ca9526792c876c42
CesarMacias94 <c.maciasvargas@campus.unimib.it>
2017-02-15 11:25:29 +0100

    Add sendMail Method

--- FlySMM/src/main/java/promotion/SeasonPromotion.java
+++ FlySMM/src/main/java/promotion/SeasonPromotion.java
@@ -6,11 +6,17 @@ public class SeasonPromotion extends Promotion{
 	private Date startDate;
 	private Date expireDate;
 	
-	public String notify_() { return "Ci sono nuove promozioni in questo periodo!"; }
+	public void notify_() {
+		this.sendMail("mail");
+		}
+	
+	public SeasonPromotion(){}
+	
 	public SeasonPromotion(Date startDate, Date expireDate){
 		this.setStartDate(startDate);
 		this.setExpireDate(expireDate);
 	}
+	
 	public SeasonPromotion(int discountRate, boolean fidelity, long idPromo, String name, Date startDate, Date expireDate){
 		this.discountRate = discountRate;
 		this.fidelity = fidelity;
@@ -19,15 +25,19 @@ public class SeasonPromotion extends Promotion{
 		this.setStartDate(startDate);
 		this.setExpireDate(expireDate);
 	}
+	
 	public Date getStartDate() {
 		return startDate;
 	}
+	
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+	
 	public Date getExpireDate() {
 		return expireDate;
 	}
+	
 	public void setExpireDate(Date expireDate) {
 		this.expireDate = expireDate;
 	}
