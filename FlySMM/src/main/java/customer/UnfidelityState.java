423c96b3a1a08dc5801c7f81bf0cddd033a23899
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-02-15 09:46:22 +0100

    edit changeFidelity method

--- FlySMM/src/main/java/customer/UnfidelityState.java
+++ FlySMM/src/main/java/customer/UnfidelityState.java
@@ -1,15 +1,23 @@
 package customer;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class UnfidelityState extends State {
 
+	private Date actualDateUnfidelity;
+	
 	public UnfidelityState(FidelityCustomer c) {
 		super(c);
+		this.actualDateUnfidelity= new Date(); 
+
 
 	}
-
 	@Override
 	public void changeFidelity() {
-		
+		if ((getDateDiff(this.actualDateUnfidelity, c.startDate, TimeUnit.MINUTES)/ (1000*60*60*24)) > (365*2)) {
+			c.setFidelity(new UnfidelityState(c));
+		}
 		 
 		
 	}
