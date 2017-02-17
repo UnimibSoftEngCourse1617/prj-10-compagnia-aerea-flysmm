3b6ebed84c287c4ef6e616ea1aff6177cc6bf53d
Andrea Biaggi <a.biaggi1@campus.unimib.it>
2017-02-17 13:44:40 +0100

    Create new classes
    Create class price
    Create seat price

--- FlySMM/src/main/java/sale/Ticket.java
+++ FlySMM/src/main/java/sale/Ticket.java
@@ -1,25 +1,44 @@
 package sale;
+
 import booking.Book;
 import booking.Passenger;
 
 public class Ticket {
 	private Passenger passenger;
 	private Book book;
+	private Seat seat;
 
-	public Ticket(Passenger passenger, Book book) {
-		super();
+	public Ticket() {
+	}
+
+	public Ticket(Passenger passenger, Book book, Seat seat) {
 		this.passenger = passenger;
 		this.book = book;
+		this.seat = seat;
 	}
 
 	public Passenger getPassenger() {
 		return passenger;
 	}
-	
-	public Book getBook(){
+
+	public void setPassenger(Passenger passenger) {
+		this.passenger = passenger;
+	}
+
+	public Book getBook() {
 		return book;
 	}
-	
-	
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public Seat getSeat() {
+		return seat;
+	}
+
+	public void setSeat(Seat seat) {
+		this.seat = seat;
+	}
 
 }
