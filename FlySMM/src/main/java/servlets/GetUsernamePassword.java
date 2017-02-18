fa54d2905a37b2e59f08a41a0d3b23cd86c3085b
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-02-18 12:02:39 +0100

    servlet request email, passwors

---
+++ FlySMM/src/main/java/servlets/GetUsernamePassword.java
@@ -0,0 +1,69 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Query;
+import org.hibernate.criterion.SizeExpression;
+import org.hibernate.mapping.List;
+
+/**
+ * Servlet implementation class GetUsernamePassword
+ */
+
+public class GetUsernamePassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GetUsernamePassword() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("email");
+		String password = request.getParameter("pws");
+
+		System.out.println("username: " + username);
+		System.out.println("password: " + password);
+		
+	
+		PrintWriter writer = response.getWriter();
+
+		// build HTML code
+		String htmlRespone = "<html>";
+		htmlRespone += "<h2>Your username is: " + username + "<br/>";
+		htmlRespone += "Your password is: " + password + "</h2>";
+		htmlRespone += "</html>";
+
+		// return response
+		writer.println(htmlRespone);
+		
+		
+	}
+
+}
