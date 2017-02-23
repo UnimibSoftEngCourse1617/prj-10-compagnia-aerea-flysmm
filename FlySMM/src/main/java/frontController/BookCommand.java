5663bd75d23572f700af2be3f3a5afa64728f918
Davide Bolzoni <davide.bolzoni59@gmail.com>
2017-02-23 11:37:22 +0100

    add update remaning seat and change creation bookId

--- FlySMM/src/main/java/frontController/BookCommand.java
+++ FlySMM/src/main/java/frontController/BookCommand.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 import booking.Baggage;
@@ -19,10 +20,16 @@ public class BookCommand extends FrontCommand {
 
 	@Override
 	public void dispatch() throws ServletException, IOException {
+		Flight departure = (Flight) request.getSession().getAttribute("chosenDeparture");
+		Flight arrival = (Flight) request.getSession().getAttribute("chosenReturn");
 		ArrayList<Book> listBook = (ArrayList<Book>) request.getSession().getAttribute("listBook");
 		for (Book b : listBook) {
 			writeBook(b);
 		}
+		for (Book b : listBook) {
+			updateSeat(b, departure, arrival);
+		}
+
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/success.html");
 		requestDispatcher.forward(request, response);
 
@@ -33,7 +40,22 @@ public class BookCommand extends FrontCommand {
 		session.getTransaction().begin();
 		session.save(b);
 		session.getTransaction().commit();
-
 	}
 
+	public static void updateSeat(Book b, Flight d, Flight a) {
+		int seat;
+		String flightId = b.getFlightId();
+		if (d.getIdFlight().equals(flightId)) {
+			seat = d.getRemainingSeats() - 1;
+		} else {
+			seat = a.getRemainingSeats() - 1;
+		}
+		Session session = SessionFactorySingleton.getSessionFactory().getCurrentSession();
+		session.getTransaction().begin();
+		String hql = "UPDATE Flight set Seat_Remaining = " + seat + " WHERE Flight_ID = '" + flightId + "'";
+		Query query = session.createQuery(hql);
+		query.executeUpdate();
+		session.getTransaction().commit();
+
+	}
 }
