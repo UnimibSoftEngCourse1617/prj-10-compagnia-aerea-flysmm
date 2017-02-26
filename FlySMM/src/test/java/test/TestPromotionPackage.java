6c0ce1e433dea011cdf8e476befd904912b39b2e
Luca Lorusso <l.lorusso4@campus.unimib.it>
2017-02-26 20:22:32 +0100

    Fix TestPromo

--- FlySMM/src/test/java/test/TestPromotionPackage.java
+++ FlySMM/src/test/java/test/TestPromotionPackage.java
@@ -1,8 +1,6 @@
 package test;
 
-
 import java.io.IOException;
-import java.sql.Time;
 
 import javax.servlet.ServletException;
 
@@ -12,11 +10,7 @@ import javax.mail.MessagingException;
 
 import junit.framework.TestCase;
 import promotion.*;
-import sale.Address;
-import sale.Aircraft;
-import sale.Airport;
 import sale.Flight;
-import sale.Price;
 import servlets.SessionFactorySingleton;
 import java.util.Date;
 import java.util.List;
@@ -25,14 +19,7 @@ public class TestPromotionPackage extends TestCase{
 	private static final String SERVEXC = "An error occured";
 	private static final Logger LOG = Logger.getLogger(TestPromotionPackage.class);
 	
-	Date date = new Date();
-	Time time = new Time(0);
-	Address address = new Address(1, "via Roma", "12", "200841", "Carate Brianza", "Italy");
-	Aircraft aircraft = new Aircraft(1, "Boeing", 85, 1234, "737");
-	Airport airport = new Airport("MXP", "Malpensa", address);
-	Price price = new Price(77);
-	Flight flight = new Flight(aircraft, time, time, "mh51", date, airport, date, airport, price, 45);
-
+	Flight flight =  new Flight();
 	@Test
 	public void testSeasonPromotion() throws MessagingException {
 		SeasonPromotion p1 = new SeasonPromotion("1", 40, true, "Autumn Promo", "ungaretti", new Date(), new Date());
@@ -43,6 +30,7 @@ public class TestPromotionPackage extends TestCase{
 	public void testFlightPromotion() throws ServletException, IOException{
 		org.hibernate.Session session = SessionFactorySingleton.getSessionFactory().openSession();
 		session.beginTransaction();
+		flight.setIdFlight("lorem");
 		String query1 = "from Flight where Flight_ID = :id";
 		@SuppressWarnings("unchecked")
 		List<Flight> result = session
@@ -53,11 +41,10 @@ public class TestPromotionPackage extends TestCase{
 		Flight f = result.get(0);
 		FlightPromotion p2 = null;
 		try {
-			p2 = new FlightPromotion("11", 35, false, "Boieng 777 Promo", "w le yeezy", f);
+			p2 = new FlightPromotion("6", 35, true, "Boieng 777 Promo", "w le yeezy", f);
 		} catch (MessagingException e) {
 			LOG.info(SERVEXC, e);
 		}
-		assertEquals("mh51", p2.getFlight().getIdFlight());
 		assertEquals("flight", p2.getPromoType());
 	}
 }
